56c320c455701eaf2ce25b88825589218e9361c6
Cybernetic1 <generic.intelligence@gmail.com>
2019-03-04 15:34:40 +0800

    experimenting with Rete

--- genifer6/genifer.py
+++ genifer6/genifer.py
@@ -4,30 +4,22 @@ from rete.network import Network
 
 def init_network():
     net = Network()
-    c0 = Has('$x', 'on', '$y')
-    c1 = Has('$y', 'left-of', '$z')
-    c2 = Has('$z', 'color', 'red')
-    net.add_production(Rule(c0, c1, c2))
+    c0 = Has('$x', 'loves', '$y')
+    # c1 = Has('$z', 'loves', 'Mary')
+    net.add_production(Rule(c0))
     return net
-
 
 def add_wmes():
     net = init_network()
     wmes = [
-        WME('B1', 'on', 'B2'),
-        WME('B1', 'on', 'B3'),
-        WME('B1', 'color', 'red'),
-        WME('B2', 'on', 'table'),
-        WME('B2', 'left-of', 'B3'),
-        WME('B2', 'color', 'blue'),
-        WME('B3', 'left-of', 'B4'),
-        WME('B3', 'on', 'table'),
-        WME('B3', 'color', 'red')
+        #WME('John', 'loves', 'Mary'),
+        #WME('Mary', 'loves', 'Pete'),
+        #WME('Mary', 'loves', 'John')
     ]
     for wme in wmes:
         net.add_wme(wme)
     print(net.dump())
 
-print("Welcome to Genifer")
+print(u"\n\u001b[32m——`—,—{\u001b[31;1m@\u001b[0m\n")
 init_network()
 add_wmes()
