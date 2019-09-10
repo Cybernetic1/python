32427cc0de054bf3189b441d6c394e07ccd63b66
Cybernetic1 <generic.intelligence@gmail.com>
2019-09-10 08:52:29 +0800

    to Python 3

--- genifer6/genifer.py
+++ genifer6/genifer.py
@@ -7,29 +7,30 @@ from rete.common import Has, Rule, WME
 from rete.network import Network
 
 def init_network():
-    net = Network()
-    c1 = Has('□', '$x', '$x')
-    c2 = Has('X', '$y', '$y')
-    net.add_production(Rule(c1, c2))
-    return net
+	net = Network()
+	c1 = Has('O', '$x', '$x')
+	c2 = Has('□', '$y', '$z')
+	c3 = Has('>', '$y', '$z')
+	net.add_production(Rule(c1, c2, c3))
+	return net
 
 def add_wmes():
-    net = init_network()
-    wmes = [
+	net = init_network()
+	wmes = [
 		WME('X', '0', '2'),
 		WME('X', '1', '1'),
 		WME('X', '2', '1'),
-        WME('O', '0', '0'),
-        WME('O', '1', '0'),
-        WME('O', '1', '2'),
-        WME('O', '2', '2'),
-        WME('□', '0', '1'),
-        WME('□', '2', '0'),
-    ]
-    for wme in wmes:
-        net.add_wme(wme)
+		WME('O', '0', '0'),
+		WME('O', '1', '0'),
+		WME('O', '1', '2'),
+		WME('O', '2', '2'),
+		WME('□', '0', '1'),
+		WME('□', '2', '0'),
+	]
+	for wme in wmes:
+		net.add_wme(wme)
 
-print(u"\n\u001b[32m——`—,—{\u001b[31;1m@\u001b[0m\n".encode("utf-8"))   # Genifer logo ——`—,—{@
+print("\n\x1b[32m——`—,—{\x1b[31;1m@\x1b[0m\n")   # Genifer logo ——`—,—{@
 
 rete_net = init_network()
 
--- genifer6/rete/pnode.py
+++ genifer6/rete/pnode.py
@@ -3,27 +3,27 @@ from rete.common import BetaNode, Token
 
 class PNode(BetaNode):
 
-    kind = 'p'
+	kind = 'p'
 
-    def __init__(self, children=None, parent=None, items=None, **kwargs):
-        """
-        :type items: list of Token
-        """
-        super(PNode, self).__init__(children=children, parent=parent)
-        self.items = items if items else []
-        self.children = children if children else []
-        for k, v in kwargs.iteritems():
-            setattr(self, k, v)
+	def __init__(self, children=None, parent=None, items=None, **kwargs):
+		"""
+		:type items: list of Token
+		"""
+		super(PNode, self).__init__(children=children, parent=parent)
+		self.items = items if items else []
+		self.children = children if children else []
+		for k, v in kwargs.items():
+			setattr(self, k, v)
 
-    def left_activation(self, token, wme, binding=None):
-        """
-        :type wme: WME
-        :type token: Token
-        :type binding: dict
-        """
-        new_token = Token(token, wme, node=self, binding=binding)
-        self.items.append(new_token)
-        print "**** firing, new token = %s\n" % new_token
+	def left_activation(self, token, wme, binding=None):
+		"""
+		:type wme: WME
+		:type token: Token
+		:type binding: dict
+		"""
+		new_token = Token(token, wme, node=self, binding=binding)
+		self.items.append(new_token)
+		print("**** firing %s\n" % new_token)
 
-    def execute(self, *args, **kwargs):
-        raise NotImplementedError
+	def execute(self, *args, **kwargs):
+		raise NotImplementedError
