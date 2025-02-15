8ac129556bcdd1775cb2bc1d095844d0358fc5fc
Cybernetic1 <generic.intelligence@gmail.com>
2025-02-15 19:14:08 +0800

    translate-file with 3 languages

--- translate-file.py
+++ translate-file.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-# Translate PLAIN text using Google Translate web page
+# Translate PLAIN text using Google Translate API
 
 # TO-DO:
 
@@ -12,9 +12,6 @@ import re
 import pyperclip
 import sys
 from os import path
-import mss			# for screen capture
-import numpy		# for analyzing image
-# from bs4 import BeautifulSoup
 import asyncio
 
 from googletrans import Translator
@@ -22,16 +19,20 @@ translator = Translator(service_urls=['translate.googleapis.com'])
 
 path = '/home/yky/Downloads/french/'
 if len(sys.argv) < 2:
-	print("usage: translate-plain in-file [out-file]")
+	print("usage: translate-plain number OR in-file out-file")
 	exit(0)
 elif len(sys.argv) == 2:
 	f1 = open(path + 'fr' + sys.argv[1] + '.txt', 'r')
-	outfile = path + 'en' + sys.argv[1] + '.txt'
+	outfile = path + 'b' + sys.argv[1] + '.html'
 	print("NOTE: output file =", outfile, end='\n\n')
 	f2 = open(outfile, 'w')
 else:
 	f1 = open(sys.argv[1], 'r')
 	f2 = open(sys.argv[2], 'w')
+
+with open("preamble.html") as f0:
+	for line in f0:
+		f2.write(line)
 
 async def do_it():
 
@@ -60,45 +61,30 @@ async def do_it():
 			# f2.write(line)
 			continue
 
-
 		# non-English texts
 		# if re.search(u'[\u4e00-\u9fff]', line):
 		#	continue
 
 		# **** Pass the line to Google
-
-		# pyperclip.copy(line)
-
-		# translate 佢!
-
-		# check the "star" icon on the right, see if it is grey
-
-		# copy text
-		time.sleep(2)
-
-		# 写鸠佢落file佬!
-		# translated = pyperclip.paste()
-
-		# 如果好似唔撚掂，停低一阵：
-		"""
-		if re.search(u'[\u4e00-\u9fff]', translated):
-			call(["beep", "-l", "500", "-f", "512"])
-			print("Error in this line:")
-			print('**** ' + last_line)
-			input("你可以自己手動翻譯....")
-			translated = pyperclip.paste()
-		"""
-
-		translation = await translator.translate(line, src='fr', dest='en')
+		translation = await translator.translate(line.rstrip(), src='fr', dest='en')
 		# for translation in translations:
-		print(translation.origin, end="")
-		f2.write(translation.text + "\n")
+		print(translation.origin)
+		f2.write("<p class='FR'>" + translation.origin + "</p>\n")
+		english = translation.text
+		f2.write("<p class='EN'>" + english + "</p>\n")
+
+		translation = await translator.translate(line, src='fr', dest='zh')
+		chinese = translation.text
+		f2.write("<p class='CN'>" + chinese + "</p><br>\n")
 
 		# print('\x1b[31m⏹ ' + line, end='\x1b[0m\n')
-		print('\x1b[33m● ' + translation.text, end='\n\x1b[0m\n')
+		print('\x1b[33m● ' + english, end='\n\x1b[0m')
+		print('\x1b[36m● ' + chinese, end='\n\x1b[0m\n')
+
 		call(["beep", "-l", "50", "-f", "3000", "-n", "-l", "50", "-f", "2500"])
 
 	f1.close()
+	f2.write("</body></html>")
 	f2.close()
 
 	call(["beep", "-l", "500", "-f", "512"])
